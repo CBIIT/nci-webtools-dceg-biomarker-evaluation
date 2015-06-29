0f50228700466d3ea18106fbb175a0b1636ffaea
Tony Hall <tony.hall@nih.gov>
2015-06-29 17:09:28 -0400

    adding sampleSize scripts to require config

--- biomarkerTools/scripts/requires.js
+++ biomarkerTools/scripts/requires.js
@@ -4,6 +4,7 @@ requirejs.config({
     jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min",
     'jquery.ui': "//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min",
     bootstrap: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min",
+    sampleSize: 'sampleSize',
     glossary: '/common/js/meansToRiskGlossary',
     app: 'app',
     routes: 'routes'
@@ -16,6 +17,9 @@ requirejs.config({
       deps: ['jquery.ui']
     },
     glossary: {
+      deps: ['jquery']
+    },
+    sampleSize: {
       deps: ['jquery']
     },
     routes: {
