37e3e7c3bc9861c69847cc1e24f95c0beca03bb5
Tony Hall <tony.hall@nih.gov>
2015-07-10 16:24:06 -0400

    pulled in mrs scripts,working on gulpfile concatenating mrs scripts and placing into correct folder, renaming files to keep filenames consistent

---
+++ biomarkerTools_testMods/src/app/scripts/routes.js
@@ -0,0 +1,22 @@
+var default_ajax_error;
+$(document).ready(function(){
+  document.title = "Biomarker Tools: Home";
+});
+$(document).on('show.bs.tab', function(el){
+  var id, title;
+  id = el.target.dataset.target.replace('#', '');
+  require([id]);
+  title = "Biomarker Tools: " + el.target.text;
+  document.title = title;
+});
+$('.goToTab').on('click', function(el){
+  var ref;
+  $('.nav li.active').removeClass('active');
+  ref = $(this).attr('data-target');
+  $(".nav a[data-target='" + ref + "']").tab('show').parent().addClass('active');
+  window.scrollTo(0, 0);
+});
+default_ajax_error = function(request, status, error){
+  $('#spinner').addClass('hide');
+  alert(request.responseText);
+};
