79c10897b53fc793be5f724ac7944863db839ef4
Tony Hall <tony.hall@nih.gov>
2015-07-13 17:29:40 -0400

    goToHelp event listener for Help link inside tab content

--- biomarkerTools_testMods/routes.js
+++ biomarkerTools_testMods/routes.js
@@ -1,7 +1,9 @@
 var default_ajax_error;
+
 $(document).ready(function(){
   document.title = "Biomarker Tools: Home";
 });
+
 $(document).on('show.bs.tab', function(el){
   var id, title;
   id = el.target.dataset.target.replace('#', '');
@@ -9,6 +11,7 @@ $(document).on('show.bs.tab', function(el){
   title = "Biomarker Tools: " + el.target.text;
   document.title = title;
 });
+
 $('.goToTab').on('click', function(el){
   var ref;
   $('.nav li.active').removeClass('active');
@@ -16,6 +19,17 @@ $('.goToTab').on('click', function(el){
   $(".nav a[data-target='" + ref + "']").tab('show').parent().addClass('active');
   window.scrollTo(0, 0);
 });
+
+$('.goToHelp').on('click', function(el){
+  var ref;
+  $('.nav li.active').removeClass('active');
+  ref = '#' + $(this).attr('data-target');
+  $(".nav a[data-target='#help']").tab('show').parent().addClass('active');
+  $('html, body').animate({
+    scrollTop: $(ref).offset().top
+  }, 2000);
+});
+
 default_ajax_error = function(request, status, error){
   $('#spinner').addClass('hide');
   alert(request.responseText);
