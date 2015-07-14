e878eedc0eac73550ecc0c72ccc21ec542b9f450
Tony Hall <tony.hall@nih.gov>
2015-07-14 14:29:18 -0400

    jump to help tab

--- biomarkerTools_testMods/src/app/scripts/routes.js
+++ biomarkerTools_testMods/src/app/scripts/routes.js
@@ -1,36 +1,43 @@
 var default_ajax_error;
 
+$.fn.goTo = function() {
+    var selector = this.selector.replace("#","");
+    document.getElementById(selector).scrollIntoView; // for chaining...\ 
+    
+    $('html, body').animate({
+        scrollTop: $(this).offset().top + 'px'
+    }, 'fast');
+    
+    return this;
+};
+
 $(document).ready(function(){
-  document.title = "Biomarker Tools: Home";
+    document.title = "Biomarker Tools: Home";
 });
 
 $(document).on('show.bs.tab', function(el){
-  var id, title;
-  id = el.target.dataset.target.replace('#', '');
-  require([id]);
-  title = "Biomarker Tools: " + el.target.text;
-  document.title = title;
+    var id, title;
+    id = el.target.dataset.target.replace('#', '');
+    require([id]);
+    title = "Biomarker Tools: " + el.target.text;
+    document.title = title;
 });
 
 $('.goToTab').on('click', function(el){
-  var ref;
-  $('.nav li.active').removeClass('active');
-  ref = $(this).attr('data-target');
-  $(".nav a[data-target='" + ref + "']").tab('show').parent().addClass('active');
-  window.scrollTo(0, 0);
+    var ref;
+    $('.nav li.active').removeClass('active');
+    ref = $(this).attr('data-target');
+    $.when$((".nav a[data-target='" + ref + "']").tab('show').parent().addClass('active')).then($(ref).goTo());
 });
 
 $('.goToHelp,.goToGlossary').on('click', function(el){
-  var ref;
-  $('.nav li.active').removeClass('active');
-  ref = '#' + $(this).attr('data-target');
-  $(".nav a[data-target='#help']").tab('show').parent().addClass('active');
-  $('html, body').animate({
-    scrollTop: $(ref).offset().top
-  }, 2000);
+    var ref;
+    $('.nav li.active').removeClass('active');
+    ref = '#' + $(this).attr('data-target');
+    $.when($(".nav a[data-target='#help']").tab('show').parent().addClass('active')).then($(ref).goTo());
 });
 
 default_ajax_error = function(request, status, error){
-  $('#spinner').addClass('hide');
-  alert(request.responseText);
+    $('#spinner').addClass('hide');
+    alert(request.responseText);
 };
