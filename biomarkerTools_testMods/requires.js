f43e44adbd5250f927ef4df0353e2fcdd16c137a
Tony Hall <tony.hall@nih.gov>
2015-07-15 17:33:32 -0400

    minor formating changes

--- biomarkerTools_testMods/requires.js
+++ biomarkerTools_testMods/requires.js
@@ -1,42 +1,46 @@
 requirejs.config({
-  paths: {
-    jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min",
-    'jquery.ui': "//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min",
-    bootstrap: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min",
-    sampleSize: 'sampleSize/sampleSize',
-    bc: 'bc/bc',
-    meanstorisk: 'meanstorisk/meanstorisk',
-    riskStratAdvanced: 'riskStratAdvanced/riskStratAdvanced',
-    meanRiskStratification: 'meanRiskStratification/meanRiskStratification',
-    glossary: '/common/js/meansToRiskGlossary',
-    routes: 'routes'
-  },
-  shim: {
-    'jquery.ui': {
-      deps: ['jquery']
+    paths: {
+        jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min",
+        'jquery.ui': "//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min",
+        bootstrap: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min",
+        help: 'help/help',
+        sampleSize: 'sampleSize/sampleSize',
+        bc: 'bc/bc',
+        meanstorisk: 'meanstorisk/meanstorisk',
+        riskStratAdvanced: 'riskStratAdvanced/riskStratAdvanced',
+        meanRiskStratification: 'meanRiskStratification/meanRiskStratification',
+        glossary: '/common/js/meansToRiskGlossary',
+        routes: 'routes'
     },
-    bootstrap: {
-      deps: ['jquery']
-    },
-    glossary: {
-      deps: ['jquery']
-    },
-    sampleSize: {
-      deps: ['jquery.ui', 'bootstrap']
-    },
-    meanstorisk: {
-      deps: ['jquery.ui', 'bootstrap']
-    },
-    riskStratAdvanced: {
-      deps: ['bootstrap']
-    },
-    routes: {
-      deps: ['bootstrap', 'glossary']
+    shim: {
+        'jquery.ui': {
+            deps: ['jquery']
+        },
+        bootstrap: {
+            deps: ['jquery']
+        },
+        glossary: {
+            deps: ['jquery']
+        },
+        sampleSize: {
+            deps: ['jquery.ui', 'bootstrap']
+        },
+        meanstorisk: {
+            deps: ['jquery.ui', 'bootstrap']
+        },
+        riskStratAdvanced: {
+            deps: ['bootstrap']
+        },
+        routes: {
+            deps: ['bootstrap', 'glossary']
+        },
+        help:{
+            deps: ['glossary']
+        }
     }
-  }
 });
 
 require(['routes'], function(){
-  console.log("default scripts loaded");
-  return {};
+    console.log("default scripts loaded");
+    return {};
 });
