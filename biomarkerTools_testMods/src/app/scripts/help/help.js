f78261417777b40edc9f598b6b47ecbe57c2d592
Tony Hall <tony.hall@nih.gov>
2015-07-15 17:34:41 -0400

    populating table of contents based on current header elements

--- biomarkerTools_testMods/src/app/scripts/help/help.js
+++ biomarkerTools_testMods/src/app/scripts/help/help.js
@@ -1,2 +1,21 @@
 // pull in glossary js file
 requirejs([ 'glossary' ]);
+var thisTool = $('#help');
+
+thisTool.find('h4, h5').each(tableOfContentsList);
+$('.goToTopic').on('click', function(){
+    $.goTo(this.data.target);
+});
+
+
+// used to construct links list for table of contents section
+function tableOfContentsList(){
+    var text = this.innerHTML;
+    if(this.tagName == "H4")
+        thisTool.find('#toc').append("<li><strong>" + text + "<strong></li>");
+    if(this.tagName == "H5"){
+        var el = "<li><a class='goToTopic' href='javascript:void(0);' data-target='#" + this.id + "'>" + text + "</a></li>";
+        if(this.id.indexOf("help_") != -1)
+            thisTool.find('#toc li:contains("Tools Help")').append(el);
+    }
+}
