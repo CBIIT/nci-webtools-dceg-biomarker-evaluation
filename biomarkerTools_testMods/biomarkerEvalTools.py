d0d2932f4ff95cb50ba9198da68aa250ee309639
Tony Hall <tony.hall@nih.gov>
2015-07-15 17:42:22 -0400

    applying port and importing scripts

--- biomarkerTools_testMods/biomarkerEvalTools.py
+++ biomarkerTools_testMods/biomarkerEvalTools.py
@@ -1,8 +1,10 @@
-from meanstorisk.meanstorisk import meanstorisk
-from bc.bc import bc
-from riskStratAdvanced import rsa
-
+from meanstorisk.meanstorisk import jsonp as meanstoriskRest
+from bc.bc import jsonp as bcRest
+from riskStratAdvanced import jsonp as riskStratAdvRest
+from sampleSize.sampleSize import sampleSizeRest as sampleSizeRest
+from meanRiskStratification.mrs import mrsRest
 from flask import Flask, url_for
+
 app = Flask(__name__, static_folder='', static_url_path='/')
 
 @app.route('/')
@@ -19,25 +21,26 @@ def api_tool_calls():
 
 # determine what python script to access and which method to call
 @app.route('/biomarkerToolsRest/<methodCall>', methods = ['GET','POST'])
-def route_tunnel(methodCall)
-    if methodCall == 'bcRest'
-        @bc.jsonp()
-    if methodCall == 'riskStratAdvRest/cal'
-        @rsa.jsonp()
-    if methodCall == 'meanstoriskRest'
-        @meanstorisk.jsonp()
-    if methodCall == 'sampleSizeRest'
-        @ss.sampleSizeRest()
-    if methodCall == 'mrsRest'
-        @mrs.mrsRest()
+def route_tunnel(methodCall):
+    if methodCall == 'bcRest':
+        bcRest
+    if methodCall == 'riskStratAdvRest/cal':
+        riskStratAdvRest
+    if methodCall == 'meanstoriskRest':
+        meanstoriskRest
+    if methodCall == 'sampleSizeRest':
+        sampleSizeRest
+    if methodCall == 'mrsRest':
+        mrsRest
         
 import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", dest="port_number", default="<port#>", help="Sets the Port")
+    # sandbox 9160, dev 8160
+    parser.add_argument("-p", dest="port_number", default="8160", help="Sets the Port")
 
     args = parser.parse_args()
     port_num = int(args.port_number);
-	
+
     hostname = gethostname()
     app.run(host='0.0.0.0', port=port_num, debug = True)
