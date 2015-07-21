7d82ec72e1dd8a594cf39342464f847902bc95a8
Tony Hall <tony.hall@nih.gov>
2015-07-21 09:39:30 -0400

    correcting service request url

--- biomarkerTools_testMods/src/app/scripts/sampleSize/sampleSize.js
+++ biomarkerTools_testMods/src/app/scripts/sampleSize/sampleSize.js
@@ -26,7 +26,7 @@ $(function(){
             }),
             // This is the type of data expected back from the server.
             dataType: 'json',
-            url: 'biomarkerToolsRest/sampleSizeRest/',
+            url: 'biomarkerToolsRest/sampleSize/',
             success: function (ret) {
                 thisTool.find("#spinner").hide();
                 thisTool.find("#output_graph").empty();
