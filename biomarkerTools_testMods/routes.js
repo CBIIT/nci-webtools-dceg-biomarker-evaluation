73b01b085ecfe4cfd06debf3392bdc1a3438e5a2
Tony Hall <tony.hall@nih.gov>
2015-07-21 10:40:41 -0400

    intializing static service variable

--- biomarkerTools_testMods/routes.js
+++ biomarkerTools_testMods/routes.js
@@ -1,4 +1,8 @@
 var default_ajax_error;
+
+
+var rest = "biomarkerToolsRest";
+
 var custom_po_tmpl = "<div class='popover' role='tooltip'><div class='arrow'></div><h3 class='popover-title'></h3><div class='popover-content'></div></div>";
 
 $(document).ready(function(){
