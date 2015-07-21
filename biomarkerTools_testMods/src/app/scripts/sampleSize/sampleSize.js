f10439f9e3a8188b7bc2459f2607f4f9334b60a6
Tony Hall <tony.hall@nih.gov>
2015-07-21 12:43:05 -0400

    correcting k input

--- biomarkerTools_testMods/src/app/scripts/sampleSize/sampleSize.js
+++ biomarkerTools_testMods/src/app/scripts/sampleSize/sampleSize.js
@@ -18,7 +18,7 @@ $(function(){
             // Encode data as JSON.
             //async:false,
             data: JSON.stringify({
-                k: thisTool.find("#independent").val(),
+                k: thisTool.find("#minInput").val() + "," + thisTool.find("#maxInput").val(),
                 sens: trim_spaces(thisTool.find("#sensitivity_val").text()),
                 spec: trim_spaces(thisTool.find("#specificity_val").text()),
                 prev: thisTool.find("#prevalence").val(),
