ef0bae89296bde9986bf1d4cbb35ef3c5ce19a3e
Tony Hall <tony.hall@nih.gov>
2015-07-24 09:33:32 -0400

    matching root python changes

--- biomarkerTools/src/app/scripts/meanRiskStratification/mrs.py
+++ biomarkerTools/src/app/scripts/meanRiskStratification/mrs.py
@@ -10,9 +10,11 @@ from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
 from rpy2.robjects.vectors import IntVector, FloatVector
 from socket import gethostname
 
-with open ('meanRiskStratification/MRSunconditionalToolWrapper.R') as fh:
+os.chdir('meanRiskStratification')
+with open ('MRSunconditionalToolWrapper.R') as fh:
         rcode = os.linesep.join(fh.readlines())
         wrapper = SignatureTranslatedAnonymousPackage(rcode,"wrapper")
+os.chdir('..')
 
 # Initialize the Flask application
 app = Flask(__name__)
