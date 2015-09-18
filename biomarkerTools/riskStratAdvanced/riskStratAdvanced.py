9f423b0861a4e7f2b418451b341e5eb7953961fa
Tony Hall <tony.hall@nih.gov>
2015-09-18 16:28:12 -0400

    consistent variable naming convention, removing image path since it should be apart of data when it returns

--- biomarkerTools/riskStratAdvanced/riskStratAdvanced.py
+++ biomarkerTools/riskStratAdvanced/riskStratAdvanced.py
@@ -46,33 +46,33 @@ def jsonp(func):
 # @jsonp
 def call_rsa_RFunction():
     data = request.json
-    print data
 
     returnedData = list()
 
     for x in data:
-        key=x["key"]
-        key=x["keyindex"]
+        key=x["key"] #cNPV or PPV
+        key_index=x["keyIndex"] #1 or 2
         contour = x["contour"]
-        contourtype = x["contourval"]
+        contour_type = x["contourType"] # contour dropdown values
         independent = x["independent"]
-        independenttype = x["independentval"]
-        independentMax = x["independentMax"]
-        independentMin = x["independentMin"]
+        independent_type = x["independentType"] # independent dropdown values
+        independent_max = x["independentMax"]
+        independent_min = x["independentMin"]
         fixed = x["fixed"]
-        fixedtype = x["fixedval"]
+        fixed_type = x["fixedType"] # fixed dropdown values
         unique = x["uniqueId"]
-        abreviatedkey = x["abreviatedkey"]
-        tabvalue = x["tabvalue"]
+        abreviated_key = x["abreviatedKey"]
+        tab_value = x["tabValue"]
         # add a variable for export when we start working on the export piece
 
         # we want to return data, imagepath(string) pairs
-        result = r_getname_getCalculations(independent, fixed, contour, independenttype, 
-            fixedtype, contourtype, abreviatedkey,key, unique, tabvalue)
+        #getCalculatedData("0.6,0.75,0.8,0.86,0.92","1,1.5,2,3","0.01,0.05,0.1","specificity","delta","prevalence","cNPV","cNPV",123456,"3")
+        result = r_getname_getCalculations(independent, fixed, contour, independent_type, 
+            fixed_type, contour_type, abreviated_key, key, key_index, tab_value, unique)
 
         print result
 
-        returnedData[x] = [ result['data'], result['imagePath'] ]
+        returnedData[x] = result['data']
         print returnedData
         
     jsonstring = ''.join(returnedData)
