8879ba3bf85e1baca2b5f0166e70e2ab3ff13ef3
Tony Hall <tony.hall@nih.gov>
2016-03-16 16:02:34 -0400

    hiding sample size tool to match production, enabling MRS

--- biomarkerTools/requires.js
+++ biomarkerTools/requires.js
@@ -7,7 +7,7 @@ requirejs.config({
         bootstrap: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min",
         datatables:"http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min",
         help: 'help/help',
-        sampleSize: 'sampleSize/sampleSize',
+       
         bc: 'bc/bc',
         meanstorisk: 'meanstorisk/meanstorisk',
         riskStratAdvanced: 'riskStratAdvanced/riskStratAdv',
