c706abecc292ee36cca0cb374207198cba309fe8
Tony Hall <tony.hall@nih.gov>
2016-09-26 16:32:19 -0400

    help and home page content fixes

--- biomarkerTools/glossary.js
+++ biomarkerTools/glossary.js
@@ -9,7 +9,7 @@ $.extend($_Glossary,{
 	},
   concern: {
     fullName: "Concern",
-    definition: "Increase in disease risk from testing positive. Formula: Concern = PPV-P(D+)"
+    definition: "Increase in disease risk from testing positive. Formula: Concern = PPV-P(D+)."
   },
 	CV : {
 		fullName : "Coefficient of Variation",
@@ -17,7 +17,7 @@ $.extend($_Glossary,{
 	},
   d_neg: {
     fullName: "Disease Negative (D-)",
-    definition: "Does not have disease"
+    definition: "Does not have disease."
   },
 	Delta : {
 		fullName : "Delta",
@@ -41,32 +41,32 @@ $.extend($_Glossary,{
 	},
   max_mrs: {
     fullName: "Maximum possible MRS for a disease with this prevalence",
-    definition: "Maximum possible MRS for a disease with this prevalence. Formula: max risk strat=2q(1-q)"
+    definition: "Maximum possible MRS for a disease with this prevalence. Formula: max risk strat=2q(1-q)."
   },
   m_neg: {
     fullName: "Marker Negativity (M-)",
-    definition: "Negative test result for biomarker test"
+    definition: "Negative test result for biomarker test."
   },
   mrs: {
     fullName: "Mean Risk Stratification (MRS)",
-    definition: "Average change in pretest-posttest disease risk. Formula: MRS=2tp(1-p)"
+    definition: "Average change in pretest-posttest disease risk. Formula: MRS=2tp(1-p)."
   },
   nnr: {
     fullName: "Number Needed to Recruit",
-    definition: "To detect 1 more disease case in positive group than negative group. Formula: NNR = 1/PBS"
+    definition: "To detect 1 more disease case in positive group than negative group. Formula: NNR = 1/PBS."
   },
   nns: {
     fullName: "Number Needed to Screen",
-    definition: "Definition for number needed to screen. Formula: Usual NNS = 1/RD"
+    definition: "screenThe reciprocal of the risk difference PPV-cNPV. Formula: Usual NNS = 1/RD"
   },
   npv: {
     fullName: "Negative Predictive Value (NPV)",
-    definition: "Definition for NPV"
+    definition: "Probability of not having disease, given a negative test result from biomarker. Unlike sensitivity and specificity, NPV reflects disease prevalence and is useful for risk stratification."
   },
   pbs: {
     fullName: "Population Burden Stratification (PBS)",
     definition: "Extra disease detection in positive group than negative group. " +
-    "Formula: PBS = a-b"
+    "Formula: PBS = a-b."
   },
 	PPV : {
 		fullName : "Positive Predictive Value (PPV)",
@@ -78,23 +78,23 @@ $.extend($_Glossary,{
 	},
   prob_d: {
     fullName: "Disease Positive (D+)",
-    definition: "Disease prevalence, or probability of disease"
+    definition: "Disease prevalence, or probability of disease."
   },
   prob_m: {
     fullName: "Marker Positivity (M+)",
-    definition: "Marker positivity, or probability of positive test result for biomarker"
+    definition: "Marker positivity, or probability of positive test result for biomarker."
   },
   q_sens: {
     fullName: "Quality of the sensitivity",
-    definition: "Increase in sensitivity versus a random test, fixing test positivity. Formula: Danger*=ybar=sens-p"
+    definition: "Increase in sensitivity versus a random test, fixing test positivity. Formula: Danger*=ybar=sens-p."
   },
   q_spec: {
     fullName: "Quality of the specificity",
-    definition: "Increase in specificity versus a random test, fixing test positivity"
+    definition: "Increase in specificity versus a random test, fixing test positivity."
   },
   reassurance: {
     fullName: "Reassurance",
-    definition: "Reduction in disease risk from testing negative. Formula: Reassurance = P(D+)-cNPV"
+    definition: "Reduction in disease risk from testing negative. Formula: Reassurance = P(D+)-cNPV."
   },
 	Risk : {
 		fullName : "Risk",
